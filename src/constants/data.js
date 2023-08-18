30011f2b787e8d997784818c0be4aab085ac986e
acinashChandavar7 <avinashChnadavar7@gmail.com>
2023-08-18 22:42:10 +0530

    adding Header component and css

---
+++ src/constants/data.js
@@ -0,0 +1,9 @@
+
+
+//  Header.jsx
+export const navBarLinks = [
+  { id: '#emotions', label: 'Emotions' },
+  { id: '#manifesto', label: 'Manifesto' },
+  { id: '#self-awareness', label: 'Self-awareness test' },
+  { id: '#work-us', label: 'Work With Us' },
+];
